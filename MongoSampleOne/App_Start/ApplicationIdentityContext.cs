8a02210dba50c2ccd3a6d332670835d8e4e78049
tpl15 <tpl15@TPL15>
2017-02-16 13:49:21 +0530

    Add project files.

---
+++ MongoSampleOne/App_Start/ApplicationIdentityContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MongoSampleOne.App_Start
+{
+    public class ApplicationIdentityContext : IDisposable
+    {
+        public static ApplicationIdentityContext Create()
+        {
+            // todo add settings where appropriate to switch server & database in your own application
+            var client = new MongoClient("mongodb://local:27017/");
+            var database = client.GetDatabase("MongoSamples");
+            return new ApplicationIdentityContext();
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
